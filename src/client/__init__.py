039c42650b1f8b7331bca7271f36d2fc623310c8
Nguyen Duy Minh Thong <nguyenduyminhthong@gmail.com>
2024-01-29 05:21:43 +0700

    Update client initialization to use PersistentClient

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -4,7 +4,7 @@ from database import ChromaDBAgent
 
 @st.cache_resource
 def create_client():
-    return ChromaDBAgent(st.secrets["VECTOR_DB_PATH"])
+    return ChromaDBAgent(path=st.secrets["VECTOR_DB_PATH"], client_type="PersistentClient")
 
 @st.cache_data(ttl=30)
 def get_training_data(_database_client):
