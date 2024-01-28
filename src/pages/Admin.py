f73a54ba6dbc47a09a1bfc432949a11377aa7104
Nguyen Duy Minh Thong <nguyenduyminhthong@gmail.com>
2024-01-29 04:28:40 +0700

    Add Admin page with data display

---
+++ src/pages/Admin.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+from client import create_client, get_training_data
+from utils import clear_cache
+
+
+st.set_page_config(page_title="Admin", layout="wide")
+st.sidebar.button("Refresh Program", on_click=clear_cache)
+
+database_client = create_client()
+
+df = get_training_data(database_client)
+
+st.dataframe(df)
