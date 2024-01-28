f7ded9042942815dd63935268c16f5b2dfd50108
Nguyen Duy Minh Thong <nguyenduyminhthong@gmail.com>
2024-01-29 04:34:36 +0700

    Update page layout in Chat.py and Admin.py

--- src/pages/Admin.py
+++ src/pages/Admin.py
@@ -4,7 +4,7 @@ from client import create_client, get_training_data
 from utils import clear_cache
 
 
-st.set_page_config(page_title="Admin", layout="wide")
+st.set_page_config(page_title="Admin")
 st.sidebar.button("Refresh Program", on_click=clear_cache)
 
 database_client = create_client()
