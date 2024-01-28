b1e8c653ec0d03156d2e1ab4e1d78963397b983f
Nguyen Duy Minh Thong <nguyenduyminhthong@gmail.com>
2024-01-29 04:56:01 +0700

    Update ChromaDBAgent connection string

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -4,8 +4,8 @@ from database import ChromaDBAgent
 
 @st.cache_resource
 def create_client():
-    return ChromaDBAgent(st.secrets["VECTOR_DB_HOST"])
+    return ChromaDBAgent(st.secrets["VECTOR_DB_PATH"])
 
-@st.cache_data(ttl=60)
+@st.cache_data(ttl=30)
 def get_training_data(_database_client):
     return _database_client.get_training_data()
