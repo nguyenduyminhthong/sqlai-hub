ee0ed5929431e8d2d09da4e69832911b3a0797ea
Nguyen Duy Minh Thong <nguyenduyminhthong@gmail.com>
2024-01-29 15:23:48 +0700

    Add blank line for readability

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -6,6 +6,7 @@ from database import ChromaDBAgent
 def create_client():
     return ChromaDBAgent(path=st.secrets["VECTOR_DB_PATH"], client_type="PersistentClient")
 
+
 @st.cache_data(ttl=30)
 def get_training_data(_database_client):
     return _database_client.get_training_data()
